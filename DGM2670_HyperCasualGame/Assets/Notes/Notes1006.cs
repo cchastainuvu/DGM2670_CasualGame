cd6a2995a60263d56e6eb2497c2983e8c0a98c21
Unknown <10798368@my.uvu.edu>
2018-10-06 12:46:34 -0600

    project setup, notes, and NameID

---
+++ DGM2670_HyperCasualGame/Assets/Notes/Notes1006.cs
@@ -0,0 +1,9 @@
+﻿
+//DGM2670 NOTES | OCT. 06, 2018 | HYPERCASUAL GAME 001//
+
+/*We don't want to rely on tags, because then we will have to create an "if" statement for each tag.
+ Since we're working with an entire alphabet, that would become really tedious to continue to make. */
+
+/*We could create an object, and its only identifier is the object itself, rather than a tag system.*/
+
+
---
+++ DGM2670_HyperCasualGame/Assets/Scripts/NameID.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class NameID : ScriptableObject
+//Will not be using the name as an ID, we're just going to name it.
+//This script is intended to have no contents withing the brackets.
+{}
