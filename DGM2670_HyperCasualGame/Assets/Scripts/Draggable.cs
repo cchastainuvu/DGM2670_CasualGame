7eb5755916ceeb5e8e203483c1b8459a348ccb0f
Unknown <10798368@my.uvu.edu>
2018-10-06 14:13:48 -0600

    letter matching and IDs

---
+++ DGM2670_HyperCasualGame/Assets/Scripts/Draggable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draggable : MonoBehaviour
+{
+
+	private Vector3 currentPosition;
+	private Vector3 newPosition;
+	private Camera cam;
+	
+	
+	void Start ()
+	{
+		//This will ensure the camera is set up when the scene begins.
+		cam = Camera.main;
+	}
+
+	private void OnMouseDown()
+	{
+		currentPosition = transform.position - cam.ScreenToWorldPoint(Input.mousePosition);
+	}
+
+	void OnMouseDrag()
+	{
+		newPosition = currentPosition + cam.ScreenToWorldPoint(Input.mousePosition);
+		newPosition.z = 0;
+		transform.position = newPosition;
+		//ScreentoWorld takes wherever the edge of the screen is in camera and puts into worldspace.
+	}
+}
