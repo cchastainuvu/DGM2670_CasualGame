174cd26dad1d82220dc2de3298494231f92440c6
Unknown <10798368@my.uvu.edu>
2018-10-15 00:06:13 -0600

    change background capability

---
+++ DGM2670_HyperCasualGame/Assets/Scripts/MouseClickEvent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseClickEvent : MonoBehaviour
+{
+
+    public UnityEvent Event;
+
+    public void OnMouseDown()
+    {
+        Event.Invoke();
+    }
+
+}
