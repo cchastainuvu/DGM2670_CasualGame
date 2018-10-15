c31cb2de23837ca68bb59aaacb818ce650e70440
Unknown <10798368@my.uvu.edu>
2018-10-14 21:32:35 -0600

    squares filled, instantiate

---
+++ DGM2670_HyperCasualGame/Assets/Scripts/InstantiateOnClick.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InstantiateOnClick : MonoBehaviour
+{
+
+    public Transform Fill;
+    public Transform Space;
+
+    public void OnMouseDown()
+    {
+        Instantiate(Fill,Space.transform.position,Space.transform.rotation);
+    }
+
+}
