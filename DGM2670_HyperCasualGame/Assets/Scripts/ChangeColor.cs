c18a3b198a14dc4ee6dd0969aef28ffebe8e7f34
Unknown <10798368@my.uvu.edu>
2018-10-14 20:03:20 -0600

    color change trigger fixed

--- DGM2670_HyperCasualGame/Assets/Scripts/ChangeColor.cs
+++ DGM2670_HyperCasualGame/Assets/Scripts/ChangeColor.cs
@@ -5,8 +5,24 @@ using UnityEngine;
 //Changes the object's color OnTrigger.//
 public class ChangeColor : MonoBehaviour 
 {
-    public void Invoke(SpriteRenderer obj)
+    //TEST RENDITION//
+    public void Invoke (SpriteRenderer obj)
     {
         GetComponent<SpriteRenderer>().color = obj.color;
     }
+    
+    //OLD RENDITION//
+//    private void OnTriggerEnter(Collider obj)
+//    {
+//        GetComponent <SpriteRenderer>().color =
+//            obj.GetComponent<SpriteRenderer>().color;
+//    }
+
+    
+    //NEW RENDITION//
+//    public void Invoke(SpriteRenderer obj)
+//    {
+//        GetComponent<SpriteRenderer>().color = obj.color;
+//    }
+    
 }
--- DGM2670_HyperCasualGame/Assets/Scripts/TriggerID.cs
+++ DGM2670_HyperCasualGame/Assets/Scripts/TriggerID.cs
@@ -6,16 +6,26 @@ public class TriggerID : MonoBehaviour
     public NameID ID;
     public UnityEvent OnMatch;
 
-    private void Invoke (Collider obj)
+
+    private void OnTriggerEnter(Collider obj)
     {
-        if ((ID == obj.GetComponent<ObjectID>().ID))
+        if (ID == obj.GetComponent<ObjectID>().ID)
         {
             OnMatch.Invoke();
-            print("Match");
-        }
-        if (ID != obj.GetComponent<ObjectID>().ID)
-        {
-            print("No!");
         }
     }
+
+    //NEW RENDITION//
+//    private void Invoke (Collider obj)
+//    {
+//        if ((ID == obj.GetComponent<ObjectID>().ID))
+//        {
+//            OnMatch.Invoke();
+//            print("Match");
+//        }
+//        if (ID != obj.GetComponent<ObjectID>().ID)
+//        {
+//            print("No!");
+//        }
+//    }
 }
