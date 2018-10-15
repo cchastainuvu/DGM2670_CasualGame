ef5ac2b7724ab2d7ed5456b86e1344d98537a3ce
Unknown <10798368@my.uvu.edu>
2018-10-15 00:59:43 -0600

    start menu and change sprite in progress

---
+++ DGM2670_HyperCasualGame/Assets/Scripts/ChangeSprite.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSprite : MonoBehaviour {
+
+    public void Invoke (SpriteRenderer obj)
+    {
+        GetComponent<SpriteRenderer>().sprite = obj.sprite;
+    }
+}
