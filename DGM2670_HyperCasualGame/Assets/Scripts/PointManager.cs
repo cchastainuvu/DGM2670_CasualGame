d8e779f965772eaf01bdb5d4685b346708bf0a80
Unknown <10798368@my.uvu.edu>
2018-10-17 00:25:45 -0600

    float data scriptable WinText, win message in progress

--- DGM2670_HyperCasualGame/Assets/Scripts/PointManager.cs
+++ DGM2670_HyperCasualGame/Assets/Scripts/PointManager.cs
@@ -1,29 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PointManager : MonoBehaviour
 {
-    private int count;
+    private float count;
     public Text Points;
+    public Text WinText;
+    public FloatData WinScore;
 
     void Start()
     {
         count = 0;
-        Points.text = count.ToString();
+        Points.text = count.ToString(CultureInfo.InvariantCulture);
+        
+        WinText.GetComponent<Text>().enabled = false;
     }
 
     public void AddPoints()
     {
         count = count + 10;
-        Points.text = count.ToString();
+        Points.text = count.ToString(CultureInfo.InvariantCulture);
         
     }
 
     public void TakePoints()
     {
         count = count - 10;
-        Points.text = count.ToString();
+        Points.text = count.ToString(CultureInfo.InvariantCulture);
     }
+
+//    public void Win()
+//    {
+//        if (count >= WinScore.value)
+//        {
+//            WinText.GetComponent<Text>().enabled = true;
+//        }
+//    }
 }
