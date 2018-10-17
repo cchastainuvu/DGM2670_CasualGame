645148abbf56c31123b5c34d4b45379fff295f2a
Unknown <10798368@my.uvu.edu>
2018-10-16 18:46:23 -0600

    point manager in progress

--- DGM2670_HyperCasualGame/Assets/Scripts/PointManager.cs
+++ DGM2670_HyperCasualGame/Assets/Scripts/PointManager.cs
@@ -1,10 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PointManager : MonoBehaviour 
+public class PointManager : MonoBehaviour
 {
+    private int count;
+    public Text Points;
 
-    
-    
+    void Start()
+    {
+        Points = GetComponent<Text>();
+        count = 0;
+
+    }
+
+    public void AddPoints()
+    {
+        
+    }
 }
