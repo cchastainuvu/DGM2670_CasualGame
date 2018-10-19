78789bc12c0a41d2771fb7b70658d7d7b303209f
Unknown <10798368@my.uvu.edu>
2018-10-18 22:09:20 -0600

    enable disable audio seamlessly - nearly complete

--- DGM2670_HyperCasualGame/Assets/Scripts/ChangeSprite.cs
+++ DGM2670_HyperCasualGame/Assets/Scripts/ChangeSprite.cs
@@ -1,9 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class ChangeSprite : MonoBehaviour {
+public class ChangeSprite : MonoBehaviour
+{
 
+    public UnityEvent OnStart;
+    
+    public void Start()
+    {
+        OnStart.Invoke();
+    }
+    
     public void Invoke (SpriteRenderer obj)
     {
         GetComponent<SpriteRenderer>().sprite = obj.sprite;
