45d6db3b445d05dc58d32a637d4817e89b0821af
Unknown <10798368@my.uvu.edu>
2018-10-27 19:18:47 -0600

    store setup

--- DGM2670_HyperCasualGame/Assets/Scripts/ScriptableObjects/Objects.cs
+++ DGM2670_HyperCasualGame/Assets/Scripts/ScriptableObjects/Objects.cs
@@ -1,12 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Object", menuName = "Store/Objects")]
 public class Objects : ScriptableObject
 {
-
     public List<Object> ObjectList;
     //Object is a generic term in Unity meaning it will take any object.
-    
-    
 }
