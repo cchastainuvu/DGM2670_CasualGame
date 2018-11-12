71d5b4463cdcee962c3df185ea05b0142b2efa51
Unknown <10798368@my.uvu.edu>
2018-11-12 13:43:14 -0700

    FollowMouse notes

---
+++ DGM2670_HyperCasualGame/Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour {
+//Place on 3D GameObject and change Camera to Perspective/Orthographic.
+
+	public Vector3 tempPos;
+	//Temporary position for setting the Vector3.
+	
+	private void Update()
+	{
+		tempPos = Input.mousePosition;
+		tempPos.z = 0;
+		//Sets the location of object based on set transform location.
+		transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		print(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+	}
+}
